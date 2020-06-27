64378cea06d9548a2c84ddb73823633dc48c5839
Orellana27 <63205626+JHON-E27@users.noreply.github.com>
2020-06-27 18:25:19 -0300

    Creación del proyecto

---
+++ Consolehello world/Consolehello world/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolehello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ndm, cd;
+            Console.WriteLine("Ingrese capacidad de Información almacenada en Gb");
+            cd = int.Parse(Console.ReadLine());
+            ndm = (cd * 1024) / 144;
+            Console.Write("Los microdiscos necesarios son: " +ndm.ToString());
+            Console.Read();
+        }
+    }
+}
